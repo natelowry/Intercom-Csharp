50a54a6e8509cc58789f2ee6dcd179e9d9c54070
Emmanuel CHAFFRAIX <emmanuel.chaffraix@margoconseil.com>
2015-10-07 20:10:54 +0200

    Base project

---
+++ Intercom.Csharp/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intercom.Csharp
+{
+    public class Class1
+    {
+    }
+}
